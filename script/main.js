bdf4150660ec78ed74855a34aef136332897c107
Shreyas Ponkshe <shreyasponkshe12345@gmail.com>
2020-05-28 03:27:59 +0530

    Changes

--- script/main.js
+++ script/main.js
@@ -231,7 +231,7 @@ const animationTimeline = () => {
       {
         scale: 1,
         rotationY: 0,
-        color: "#ff69b4",
+        color: "#6023EF",
         ease: Expo.easeOut
       },
       0.1,
